8a863f88c6ef2b666f814e3d34aa5fad8246ffc5
akodji <138826429+akodji@users.noreply.github.com>
2023-07-17 15:43:02 +0000

    garlandsandflowers.py

--- garlandsandflowers.py
+++ garlandsandflowers.py
@@ -1,4 +1,10 @@
 def compareStrings(garlands,flowers):
+    if len(garlands)>=1 and len(flowers)<=50:
+        print('Great!')
+    else:
+        print('Length requirement has not been met') 
+        
+        
     count=0
     for i in flowers:
         if i in garlands:
